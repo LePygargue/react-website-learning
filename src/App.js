0a84b79f09602dd40f6ee6b2464eeff185e34868
LePygargue <46059303+LePygargue@users.noreply.github.com>
2020-11-17 14:37:52 +0100

    Bug refresh

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/services' component={Services} />
-          <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/products' exact component={Products} />
+          <Route path='/sign-up' exact component={SignUp} />
         </Switch>
       </Router>
     </>
